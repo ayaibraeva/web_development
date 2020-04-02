6c5607f2defc47d438f0807c48520a2ae2b3573d
ayaibraeva <ayaulymybraeva@gmail.com>
2020-04-02 16:43:53 +0600

    web

--- store/demo/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from main.views import hello, product_list, product_details
-urlpatterns = [
-    path ('hello/', hello),
-    path('products/', product_list),
-    path ('products/<int:product_id>/', product_details),
-]
